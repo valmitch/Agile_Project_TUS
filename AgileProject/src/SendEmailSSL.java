6b21b6b1c8044db4247bb1c4efda1a20ed4e761b
Luke Fahy <a00304788@student.ait.ie>
2022-11-11 00:24:45 +0000

    email class file

---
+++ AgileProject/src/SendEmailSSL.java
@@ -0,0 +1,53 @@
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class SendEmailSSL {
+
+    public static void main(String[] args) {
+
+        final String username = "test@gmail.com";
+        final String password = "password123";
+
+        //SMTP settings
+        Properties prop = new Properties();
+		prop.put("mail.smtp.host", "smtp.gmail.com");
+        prop.put("mail.smtp.port", "465");
+        prop.put("mail.smtp.auth", "true");
+        prop.put("mail.smtp.socketFactory.port", "465");
+        prop.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        
+        Session session = Session.getInstance(prop,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(username, password);
+                    }
+                });
+        //Describes email contents
+        try {
+
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress("from@gmail.com"));
+            message.setRecipients(
+                    Message.RecipientType.TO,
+                    InternetAddress.parse("to_username_a@gmail.com, to_username_b@yahoo.com")
+            );
+            message.setSubject("Subject Test");
+            message.setText("Body Test");
+
+            Transport.send(message);
+
+            System.out.println("Email sent");
+
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
